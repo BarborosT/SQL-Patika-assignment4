f0a0c86782a9484b6ed9f4bd5b8ad09ece219a0a
BarborosT <72398699+BarborosT@users.noreply.github.com>
2022-11-27 02:28:03 +0300

    Add files via upload

---
+++ assignment4.sql
@@ -0,0 +1,9 @@
+Select Distinct(replacement_cost),* From film
+
+Select Count(Distinct(replacement_cost)) From film
+
+Select count(*) From film Where title like 'T%' AND rating='G';
+
+Select count(*) From country Where country like '_____'
+
+Select count(*) From city Where city like 'R%' or city like 'r%'
